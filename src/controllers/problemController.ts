34bfdd256e2a5dcaf0ddbbacf18086a366f794ed
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2025-01-10 21:28:15 +0700

    created seeders for users and problems, finsih 1 random problem route

--- src/controllers/problemController.ts
+++ src/controllers/problemController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
+import { ProblemService } from "../services/problemServce";
 
 export class ProblemController{
+    constructor(private problemService: ProblemService){};
+
     randomProblem = async(req: Request, res: Response) => {
         try {
-            res.status(200).json({message: "get random problem route"});
+            const randomProblem = await this.problemService.getRandomProblem();
+            res.status(200).json({
+                message: "get random problem route",
+                problem: randomProblem
+            });
         } catch (error) {
             res.status(500).json({message: "Failed to get random problem"});
         }
