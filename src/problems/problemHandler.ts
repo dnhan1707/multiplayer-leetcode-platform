9943fff5ad6a76cf5801961666133dd04533e9c0
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2025-02-06 20:26:41 +0700

    added more array problems

--- src/problems/problemHandler.ts
+++ src/problems/problemHandler.ts
@@ -5,74 +5,101 @@ export class ProblemHandler {
     private problemService = new ProblemService();
 
     modifySubmittedCode = async () => {
-        console.log("In here");
-        console.log("Problem ID", this.problem_id);
+        // console.log("In here");
+        // console.log("Problem ID", this.problem_id);
         const problemTitle = await this.problemService.getProblemTitle(this.problem_id);
         switch (problemTitle) {
             case "Two Sum":
-                const twoSum = new TwoSum();
-                return await twoSum.modifySubmittedCode(this.submitted_code);
+                return await this.modifyCode(new TwoSum());
             case "Remove Element":
-                const removeElement = new RemoveElement();
-                return await removeElement.modifySubmittedCode(this.submitted_code);
+                return await this.modifyCode(new RemoveElement());
             case "Contain Duplicate":
-                const containDuplicate = new ContainDuplicate();
-                return await containDuplicate.modifySubmittedCode(this.submitted_code);
+                return await this.modifyCode(new ContainDuplicate());
+            case "Valid Anagram":
+                return await this.modifyCode(new ValidAnagram());
+            case "Group Anagram":
+                return await this.modifyCode(new GroupAnagram());
             default:
                 return this.submitted_code;
         }
     }
+
+    private modifyCode = async (modifier: CodeModifier) => {
+        return await modifier.modifySubmittedCode(this.submitted_code);
+    }
 }
 
-export class TwoSum {
-    modifySubmittedCode = async (submittedCode: string) => {
+interface CodeModifier {
+    modifySubmittedCode(submittedCode: string): Promise<string>;
+}
+
+abstract class BaseModifier implements CodeModifier {
+    abstract modifySubmittedCode(submittedCode: string): Promise<string>;
+
+    protected generateModifiedCode(submittedCode: string, inputCode: string): string {
         if (!submittedCode) {
             throw new Error("Missing submitted code");
         }
-
-        const modifiedCode = `
+        return `
             ${submittedCode}
 
             const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
+            ${inputCode}
+        `;
+    }
+}
+
+export class TwoSum extends BaseModifier {
+    modifySubmittedCode = async (submittedCode: string) => {
+        const inputCode = `
             const [nums, target] = JSON.parse(input);
             const result = twoSum(nums, target);
             console.log(result);
         `;
-        return modifiedCode;
+        return this.generateModifiedCode(submittedCode, inputCode);
     }
 }
 
-
-export class RemoveElement {
+export class RemoveElement extends BaseModifier {
     modifySubmittedCode = async (submittedCode: string) => {
-        if (!submittedCode) {
-            throw new Error("Missing submitted code");
-        }
-        const modifiedCode = `
-            ${submittedCode}
-
-            const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
+        const inputCode = `
             const [nums, val] = JSON.parse(input);
             const result = removeElement(nums, val);
             console.log(result);
         `;
-        return modifiedCode;
+        return this.generateModifiedCode(submittedCode, inputCode);
     }
 }
 
-export class ContainDuplicate {
+export class ContainDuplicate extends BaseModifier {
     modifySubmittedCode = async (submittedCode: string) => {
-        if (!submittedCode) {
-            throw new Error("Missing submitted code");
-        }
-        const modifiedCode = `
-            ${submittedCode}
-
-            const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim();
+        const inputCode = `
             const [nums] = JSON.parse(input);
             const result = containDuplicate(nums);
             console.log(result);
         `;
-        return modifiedCode;
+        return this.generateModifiedCode(submittedCode, inputCode);
+    }
+}
+
+export class ValidAnagram extends BaseModifier {
+    modifySubmittedCode = async (submittedCode: string) => {
+        const inputCode = `
+            const [s, t] = JSON.parse(input);
+            const result = validAnagram(s, t);
+            console.log(result);
+        `;
+        return this.generateModifiedCode(submittedCode, inputCode);
+    }
+}
+
+export class GroupAnagram extends BaseModifier {
+    modifySubmittedCode = async (submittedCode: string) => {
+        const inputCode = `
+            const [strs] = JSON.parse(input);
+            const result = validAnagram(strs);
+            console.log(result);
+        `;
+        return this.generateModifiedCode(submittedCode, inputCode);
     }
 }
